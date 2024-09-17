c098950a8da7030ff259184f05a4fb07e40a5102
Shahriarsalman39 <shahriarsalman39@gmail.com>
2024-09-17 15:23:39 +0600

    f

--- src/test/java/com/facebook_Tests/CreatAccountTest.java
+++ src/test/java/com/facebook_Tests/CreatAccountTest.java
@@ -13,13 +13,13 @@ public class CreatAccountTest extends Base_Test{
 	CreateAccountPage cap;
 	TestReader tr;
 	
-	@BeforeMethod
+	@BeforeMethod(groups= {"smoke","Regression","Functional"})
 	public void openfacebook() {
 		openapplication();
 	}
 	
-	@Test
-	public void openAccountTest() {
+	@Test(groups= {"smoke","Regression","Functional"})
+	public void ValidopenAccountTest() {
 	    cap= new CreateAccountPage(driver);
 	    tr = new TestReader();
 	    
@@ -37,8 +37,58 @@ public class CreatAccountTest extends Base_Test{
 	}
 	
 	
+	@Test(groups= {"smoke","Regression"})
+	public void InvalidopenAccountTest() {
+	    cap= new CreateAccountPage(driver);
+	    tr = new TestReader();
+	    
+		cap.clickOnNewAcc();
+		
+		cap.FirstName(tr.readF_name());
+		cap.Surname(tr.readS_name());
+		cap.Email(tr.readEmail());
+		cap.password(tr.readPasS());
+		cap.clickOnDateMonthYear();
+		//cap.clickOnMonth();
+		//cap.clickOnYear();
+		cap.clickOnGender();
+		cap.clickOnSignUp();
+	}
 	
-	//@AfterMethod
+	@Test(groups= {"smoke","Functional"})
+	public void Valid2openAccountTest() {
+	    cap= new CreateAccountPage(driver);
+	    tr = new TestReader();
+	    
+		cap.clickOnNewAcc();
+		
+		cap.FirstName(tr.readF_name());
+		cap.Surname(tr.readS_name());
+		cap.Email(tr.readEmail());
+		cap.password(tr.readPasS());
+		cap.clickOnDateMonthYear();
+		
+		cap.clickOnGender();
+		cap.clickOnSignUp();
+	}
+	
+	@Test(groups= {"Regression","Functional"})
+	public void Valid3openAccountTest() {
+	    cap= new CreateAccountPage(driver);
+	    tr = new TestReader();
+	    
+		cap.clickOnNewAcc();
+		
+		cap.FirstName(tr.readF_name());
+		cap.Surname(tr.readS_name());
+		cap.Email(tr.readEmail());
+		cap.password(tr.readPasS());
+		cap.clickOnDateMonthYear();
+		
+		cap.clickOnGender();
+		cap.clickOnSignUp();
+	}
+	@AfterMethod(groups= {"smoke","Regression","Functional"})
 	public void closefacebook() {
 		closeApplication();
 	}
